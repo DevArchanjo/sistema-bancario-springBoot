d3d0ca29bb381b9c5a01be30fd7f743468d17f5c
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2026-02-10 22:49:24 -0300

    Criação do objeto de exceção ObjectNotGenerated para caso houver um erro ao gerar o numero da conta bancária

---
+++ src/main/java/com/bancoweb/banco/service/exception/ObjectNotGenerated.java
@@ -0,0 +1,9 @@
+package com.bancoweb.banco.service.exception;
+
+public class ObjectNotGenerated extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotGenerated(String msg) {
+		super(msg);
+	}
+}
