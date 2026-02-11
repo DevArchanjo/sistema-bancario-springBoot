c912624c017978da039fb64cf35ebf2d0b195f75
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2026-02-10 23:01:47 -0300

    Removendo dados sensiveis para não serem exibidos tornando o DTO mais genérico com poucas informações da conta bancária, servido para buscar uma conta

--- src/main/java/com/bancoweb/banco/dto/ContaSearchDTO.java
+++ src/main/java/com/bancoweb/banco/dto/ContaSearchDTO.java
@@ -2,30 +2,20 @@ package com.bancoweb.banco.dto;
 
 import java.io.Serializable;
 
-import com.bancoweb.banco.domain.Conta;
-
-public class ContaDTO implements Serializable {
+public class ContaSearchDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private String id;
 	private String numero;
 	private String titular;
+	private String documento;
 	
-	public ContaDTO() {
+	public ContaSearchDTO() {
 	}
 	
-	public ContaDTO(Conta obj) {
-		this.id = obj.getId();
-		this.numero = obj.getNumero();
-		this.titular = obj.getCliente().getTitular();
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
+	public ContaSearchDTO(String numero, String titular, String documento) {
+		this.numero = numero;
+		this.titular = titular;
+		this.documento = documento;
 	}
 
 	public String getNumero() {
@@ -42,5 +32,13 @@ public class ContaDTO implements Serializable {
 
 	public void setTitular(String titular) {
 		this.titular = titular;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
 	}
 }
